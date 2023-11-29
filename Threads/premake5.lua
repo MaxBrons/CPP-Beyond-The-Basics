6508de6912be7dbaadfa799f63c6e8083ce1a31c
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-11-29 11:38:34 +0100

    Setup threads project

---
+++ Threads/premake5.lua
@@ -0,0 +1,35 @@
+project "Threads-Assembly"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"src",
+		"../Core/src"
+	}
+
+	links
+	{
+		"Core"
+	}
+
+	filter "configurations:Debug"
+		defines "BTB_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "BTB_RELEASE"
+		runtime "Release"
+        	optimize "on"
