dac08d3929a4c6d15f7b2306739d0a46bf89a40f
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-12-16 00:01:22 +0100

    Created thread safety for the producer-consumer program using a condition variable

--- Threads/src/main.cpp
+++ Threads/src/main.cpp
@@ -3,55 +3,113 @@
 #include <queue>
 #include <thread>
 #include <iostream>
+#include <mutex>
 #include <condition_variable>
+#include <chrono>
 
-int counter = 0;
-bool done = false;
+const int PRODUCER_GOODS_AMOUNT = 500;
 
-std::queue<int> goods;
-//std::unique_lock<std::mutex> m_lock;
+int m_Counter = 0;
+bool m_Done = false;
+std::queue<int> m_Goods;
 
-void producer() {
-    std::cout << "Starting producer..." << std::endl;
-    
-    //if (m_lock.try_lock()) {
+std::mutex m_Mutex;
+std::condition_variable m_Condition;
 
-        for (int i = 0; i < 500; ++i) {
-            goods.push(i);
-            counter++;
-        }
-
-        done = true;
-        //m_lock.unlock();
-    //}
-
-
-    std::cout << "Finished producer..." << std::endl;
+// Easy method for printing a gui header to the console.
+inline void DisplayHeader(const std::string& header)
+{
+    std::cout << GUI::CreateHeader(header, Colour::Background::Green) << std::endl;
 }
 
-void consumer() {
-    std::cout << "Starting consumer..." << std::endl;
-
-    //if (m_lock.try_lock()) {
-        while (!done) {
-            while (!goods.empty()) {
-                goods.pop();
-                counter--;
-            }
-        }
-    //}
-
-    std::cout << "Finished consumer..." << std::endl;
+// Easy method for printing a gui sub header to the console.
+inline void DisplaySubHeader(const std::string& header)
+{
+    std::cout << GUI::CreateHeader(header, Colour::Background::Blue, 1) << std::endl;
 }
 
+// Easy method for printing to the console.
+inline void PrintLine(const std::string& body = "")
+{
+    std::cout << body << std::endl;
+}
 
-int main() {
-    counter = 0;
+// Producer function to add goods to the queue.
+void producer()
+{
+    PrintLine("1. Starting producer...");
+
+    // Produce goods and update the counter.
+    for (int i = 0; i < PRODUCER_GOODS_AMOUNT; ++i)
+    {
+        {
+            // Lock the mutex to ensure exclusive access to the goods vector.
+            std::lock_guard<std::mutex> lock(m_Mutex);
+            m_Goods.push(i);
+            m_Counter++;
+        }
+
+        // Notify the consumer that new goods are available.
+        m_Condition.notify_one();
+    }
+
+    // Notify the consumer that the producer is done producing goods.
+    m_Done = true;
+    m_Condition.notify_one();
+
+    PrintLine("3. Finished producer...");
+}
+
+// Consumer function to consume goods from the queue.
+void consumer()
+{
+    PrintLine("2. Starting consumer...");
+
+    // Continue consuming goods until the producer is done and the queue is empty.
+    while (!m_Done || !m_Goods.empty())
+    {
+        // Lock the mutex and wait for goods to be available or for the producer to be done.
+        std::unique_lock<std::mutex> lock(m_Mutex);
+        m_Condition.wait(lock, [&]() { return !m_Goods.empty() || m_Done; });
+
+        // Break the loop if the producer is done and the queue is empty.
+        if (m_Done && m_Goods.empty())
+            break;
+
+        // Consume the goods and update the counter.
+        m_Goods.pop();
+        m_Counter--;
+    }
+
+    PrintLine("4. Finished consumer...");
+}
+
+int main()
+{
+    DisplayHeader(" PRODUCER CONSUMER ");
+
+    // Record the starting time for performance measurement.
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    m_Counter = 0;
+
+    // Create producer and consumer threads.
     std::thread producerThread(producer);
     std::thread consumerThread(consumer);
 
+    // Wait for threads to finish execution.
     producerThread.join();
     consumerThread.join();
 
-    std::cout << "Net: " << counter << " " << goods.size() << std::endl;
+    // Calculate the execution time of the process.
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    PrintLine();
+
+    DisplaySubHeader("RESULTS");
+    PrintLine(GUI::CreateHeader("Produced goods: " + std::to_string(PRODUCER_GOODS_AMOUNT) + " | " + "Net should be 0.", 1));
+    PrintLine(GUI::CreateHeader("Net: " + std::to_string(m_Counter) + " | " + "Goods array size: " + std::to_string(m_Goods.size()), 1));
+    PrintLine(GUI::CreateHeader("Execution time: " + std::to_string(duration.count()) + " milliseconds", 1));
+
+    return 0;
 }
